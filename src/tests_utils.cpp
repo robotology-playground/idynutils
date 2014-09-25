b3fa37ee3aee90d643cda314d9734205fa5fe627
Alessio Rocchi <alessio.rocchi@iit.it>
2014-09-25 19:10:36 +0200

    Added methods in tests_utils to compute random frames and robot configurations

--- src/tests_utils.cpp
+++ src/tests_utils.cpp
@@ -2,27 +2,94 @@
 #include <time.h>
 #include <iostream>
 
-double tests_utils::getRandomAngle()
+void tests_utils::initializeIfNeeded()
 {
-    static bool initialized = false;
-    if(!initialized) {
+    static bool is_initialized = false;
+
+    if(!is_initialized) {
         time_t seed = time(NULL);
         seed48((unsigned short*)(&seed));
-        initialized = true;
+        srand((unsigned int)(seed));
+
+        is_initialized = true;
     }
+
+}
+
+double tests_utils::getRandomAngle()
+{
+    tests_utils::initializeIfNeeded();
     return drand48()*2.0*M_PI-M_PI;
+}
+
+yarp::sig::Vector tests_utils::getRandomAngles(const yarp::sig::Vector& min,
+                                               const yarp::sig::Vector& max,
+                                               const int size)
+{
+    tests_utils::initializeIfNeeded();
+    yarp::sig::Vector q(size);
+    assert(min.size() >= size);
+    assert(max.size() >= size);
+    for(unsigned int i = 0; i < size; ++i)
+        q(i) = getRandomAngle(min[i],max[i]);
+    return q;
 }
 
 double tests_utils::getRandomAngle(const double min, const double max)
 {
+    tests_utils::initializeIfNeeded();
+    assert(min <= max);
     if(min < -M_PI || max > M_PI)
         return getRandomAngle();
 
-    static bool initialized = false;
-    if(!initialized) {
-        srand(time(NULL));
-        initialized = true;
-    }
     return (double)rand()/RAND_MAX * (max-min) + min;
 }
+
+double tests_utils::getRandomLength(const double min, const double max)
+{
+    tests_utils::initializeIfNeeded();
+    assert(min <= max);
+    return (double)rand()/RAND_MAX * (max-min) + min;
+}
+
+KDL::Vector tests_utils::getRandomVector(const double min, const double max)
+{
+    tests_utils::initializeIfNeeded();
+
+    double x = tests_utils::getRandomLength(min,max);
+    double y = tests_utils::getRandomLength(min,max);
+    double z = tests_utils::getRandomLength(min,max);
+    KDL::Vector p(x,y,z);
+    return p;
+}
+
+KDL::Rotation tests_utils::getRandomRotation(const double min, const double max)
+{
+    tests_utils::initializeIfNeeded();
+
+    double R = tests_utils::getRandomAngle(min,max);
+    double Pmin = min; double Pmax = max;
+    if(Pmin < -M_PI_2 || Pmax > M_PI_2) { Pmin /= 2; Pmax /= 2; }
+    double P = tests_utils::getRandomAngle(Pmin,Pmax);
+    double Y = tests_utils::getRandomAngle(min,max);
+
+    KDL::Rotation rot = KDL::Rotation::RPY(R,P,Y);
+    return rot;
+}
+
+KDL::Frame tests_utils::getRandomFrame(const double lengthMin, const double lengthMax,
+                                       const double rotMin, const double rotMax)
+{
+    tests_utils::initializeIfNeeded();
+
+    KDL::Vector p = tests_utils::getRandomVector(lengthMin,
+                                                 lengthMax);
+    KDL::Rotation r = tests_utils::getRandomRotation(rotMin,
+                                                     rotMax);
+    KDL::Frame f(r,p);
+
+    return f;
+}
+
+
 
