0f2eff3641de8049bfff30c300a6c520fbaec54b
Enrico Mingo <enrico.mingo@iit.it>
2014-11-07 15:45:30 +0100

    Deleted methods of OpenSoT

--- python/pydynutils/pydrc.py
+++ python/pydynutils/pydrc.py
@@ -39,48 +39,6 @@ def KDLPose2yarpPose(kdlPose, bottle, frame="world"):
 
     return bottle
 
-def pose_msg(kdlFrame, base_frame, distal_frame, bottle):
-    bottle.clear()
-
-    p = kdlFrame.p
-    r = kdlFrame.M.GetQuaternion()
-
-    bottle.addDouble(p[0])
-    bottle.addDouble(p[1])
-    bottle.addDouble(p[2])
-    bottle.addDouble(r[0])
-    bottle.addDouble(r[1])
-    bottle.addDouble(r[2])
-    bottle.addDouble(r[3])
-    bottle.addString(base_frame)
-    bottle.addString(distal_frame)
-
-    return bottle
-
-def twist_msg(kdlTwist, base_frame, distal_frame, bottle)
-    bottle.clear()
-
-    v = kdlTwist.vel
-    w = kdlTwist.rot
-
-    bottle.addDouble(v[0])
-    bottle.addDouble(v[1])
-    bottle.addDouble(v[2])
-    bottle.addDouble(w[0])
-    bottle.addDouble(w[1])
-    bottle.addDouble(w[2])
-    bottle.addString(base_frame)
-    bottle.addString(distal_frame)
-
-    return bottle
-
-def trj_msg(kdlFrame, kdlTwist, base_frame, distal_frame, bottle)
-    bottle.clear()
-
-    pose_msg(kdlFrame, base_frame, distal_frame, bottle)
-    twist_msg(kdlTwist, base_frame, distal_frame, bottle)
-
-    return bottle
 
 def yarpListToTuple(listBottle):
     tuple = []
