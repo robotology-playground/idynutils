534016359dde459684ccaf3a748bbed3ff5a9265
Alessio Rocchi <alessio.rocchi@iit.it>
2015-02-13 12:04:48 +0100

    Fixed errors in previous commit

--- src/RobotUtils.cpp
+++ src/RobotUtils.cpp
@@ -89,9 +89,9 @@ void RobotUtils::move(const yarp::sig::Vector &_q) {
     fromIdynToRobot(_q,
                     q_commanded_right_arm,
                     q_commanded_left_arm,
-                    q_commanded_torso,
-                    q_commanded_right_leg,
-                    q_commanded_left_leg);
+                    q_commanded_torso);//,
+//                    q_commanded_right_leg,
+//                    q_commanded_left_leg);
 
     torso.move(q_commanded_torso);
     left_arm.move(q_commanded_left_arm);
