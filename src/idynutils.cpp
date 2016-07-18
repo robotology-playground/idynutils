96972f0aa3035ca7f0bbe668e94536a5f4edc914
Luca Muratore <luca.muratore@iit.it>
2016-07-18 14:40:28 +0200

    Added check on planning scene

--- src/idynutils.cpp
+++ src/idynutils.cpp
@@ -936,7 +936,9 @@ moveit_msgs::PlanningScene iDynUtils::getPlanningSceneMsg()
                             components.WORLD_OBJECT_NAMES;
     this->moveit_planning_scene->getPlanningSceneMsg(scene, components);
     #ifdef RVIZ_DOES_NOT_TRANSFORM_OCTOMAP
-    octomap_utils::octomapWithPoseToOctomap(scene.world.octomap);
+    if(this->moveit_planning_scene->getWorld()->getObject(planning_scene::PlanningScene::OCTOMAP_NS)) {
+        octomap_utils::octomapWithPoseToOctomap(scene.world.octomap);
+    }
     #endif
     return scene;
 }
